072eb5078cda3287aec27763c765794ca859925e
Md. Tanvir <tanvirhasan66270@gmail.com>
2026-05-07 17:57:04 +0600

    add

--- jee_angular_69/src/app/app.routes.ts
+++ jee_angular_69/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './component/layout/home/home';
+import { HomeFront } from './component/layout/home-front/home-front';
 
 export const routes: Routes = [
 
 
-    {path:'',component:Home}
+    {path:'',component:HomeFront}
 ];
