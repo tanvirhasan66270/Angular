037c5ab68b56c69527a61d913bf25afbaf118058
Md. Tanvir <tanvirhasan66270@gmail.com>
2026-05-09 03:32:20 +0600

    Delete jee_angular_69 directory

--- jee_angular_69/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { Home } from './component/layout/home/home';
-import { HomeFront } from './component/layout/home-front/home-front';
-
-export const routes: Routes = [
-
-
-    {path:'',component:HomeFront}
-];
--- jee_angular_69/src/model/department.model.ts
+++
@@ -1,7 +0,0 @@
-export interface departmentModel{
-
-id?:string;
-name:string;
-email:string;
-
-}
